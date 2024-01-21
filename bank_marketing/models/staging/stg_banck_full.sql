4eead3d2ed992e58b4d00dd4290fd54d5d6aadb3
Nayara Bernardo <nayyarabernardo@gmail.com>
2024-01-21 18:15:38 -0300

    Camada STG table stg_banck_full

--- bank_marketing/models/staging/stg_banck_full.sql
+++ bank_marketing/models/staging/stg_banck_full.sql
@@ -1,3 +1,5 @@
+-- models/staging/stg_banck_full.sql
+
 {{ config(materialized='table') }}
 
 with source_data as (
@@ -6,8 +8,21 @@ with source_data as (
         cast(job as STRING) as trabalho,
         cast(marital as STRING) as estado_civil,
         cast(education as STRING) as educacao,
-        {{source('bank_marketing', 'bank-full')}}
+        cast(balance as FLOAT64) as saldo_anual,
+        cast(housing as STRING) as tem_credito_habitacao,
+        cast(loan as STRING) as tem_emprestimo_pessoal,
+        cast(contact as STRING) as tipo_contato,
+        cast(day as INTEGER) as ultimo_dia_contato,
+        cast(month as STRING) as ultimo_mes_contato,
+        cast(duration as INTEGER) as duracao_ultimo_contato,
+        cast(campaign as INTEGER) as quantidade_contatos_campanha,
+        cast(pdays as INTEGER) as dias_ultima_campanha,
+        cast(previous as INTEGER) as quantidade_contatos_anteriores,
+        cast(poutcome as STRING) as resultado_campanha,
+        cast(y as STRING) as subscreveu_deposito_prazo
+    from {{ source('bank_marketing', 'bank-full') }}
 )
 
 select *
 from source_data
+
