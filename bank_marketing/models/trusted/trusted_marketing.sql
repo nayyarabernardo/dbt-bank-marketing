54a6175fb96e859838326ca750fa9276e93a7526
Nayara Bernardo <nayyarabernardo@gmail.com>
2024-01-25 22:23:43 -0300

    Modelagem de dados Camada Trusted_cliente

--- bank_marketing/models/trusted/trusted_marketing.sql
+++ bank_marketing/models/trusted/trusted_marketing.sql
@@ -1,26 +1,24 @@
+{{ config(materialized='table') }}
 
 with 
     tbmarketing as (
         select
-            tc.id_cliente,
+            id_cliente,
             tipo_contato,
-            ultimo_dia_contato,
-            ultimo_mes_contato,
+            mes_ultimo_contato,
+            dia_semana_ultimo_contato,
             duracao_ultimo_contato,
-            quantidade_contatos_campanha,
-            dias_ultima_campanha,
-            quantidade_contatos_anteriores,
+            numero_contatos_campanha,
+            dias_desde_ultimo_contato_anterior,
+            numero_contatos_anteriores,
+            resultado_campanha_anterior,
             taxa_variacao_emprego,
             indice_precos_consumidor,
             indice_confianca_consumidor,
             taxa_euribor_3meses,
             cliente_subscreveu_deposito_prazo
-        from {{ ref('stg_bank_full') }} ts
-        left join {{ ref('trusted_cliente') }} tc
-        on ts.id_cliente = tc.id_cliente
+        from {{ ref('stg_bank_full') }} 
     )
 
-select 
-    tbmarketing.*,
-    tc.id_cliente
+select *
 from tbmarketing
---
+++ bank_marketing/models/trusted/trusted_tbtipocontato.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+with 
+    tb_tipocontato as (
+        SELECT DISTINCT
+            tipo_contato
+        from {{ ref('stg_bank_full') }} 
+    )
+
+
+select *,
+    ROW_NUMBER() OVER () AS id_tipocontato,
+from tb_tipocontato
+
+ 
